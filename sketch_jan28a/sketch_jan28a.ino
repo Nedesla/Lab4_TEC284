be73bde40e999d74631d2e5d636e1e432bf1b0ab
Nedesla <nedesla@ilstu.edu>
2025-01-28 14:48:56 -0600

    Temp Display
    Added temperature and humidity and converted it to Fahrenheit.

--- sketch_jan28a/sketch_jan28a.ino
+++ sketch_jan28a/sketch_jan28a.ino
@@ -5,18 +5,30 @@
 
 U8X8_SSD1306_128X64_ALT0_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 
+DHT dht(3, DHT11);
+
 void setup() {
   // put your setup code here, to run once:
 u8x8.begin();
 u8x8.setFlipMode(1);
 u8x8.clear();
 
+dht.begin();
+
 }
 
 void loop() {
+double var1 = dht.readTemperature();
+double var2 = dht.readHumidity();
+//Coversions
+double var1F = var1 * 9/5 +32;
+double var2F = var2 * 9/5 +32;
   // put your main code here, to run repeatedly:
 u8x8.setFont(u8x8_font_chroma48medium8_r);
 u8x8.setCursor(0, 0);
-u8x8.print("Hello World!");
-
+//u8x8.print("Hello World!");
+u8x8.println("Temp:");
+u8x8.println(var1F);
+u8x8.println("Humidity:");
+u8x8.println(var2F);
 }
